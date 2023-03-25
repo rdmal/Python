0ae5caf985d75e0596cda9364299f2677c3d9f3c
Raji Mallina <99835702+rdmal@users.noreply.github.com>
2023-03-25 09:40:39 -0400

    Add files via upload

---
+++ python/StcoksList.py
@@ -0,0 +1,10 @@
+
+
+
+def main():
+  Stocks=("AAPL: MC:1984.73B, Emp:164000, Sales:394.33B, Div:0.92",
+  "GME: MC:5.20B, Emp:12000, Sales:5.95B, Div: -",
+  "AMZN: MC:877.38B, Emp:1544000, Sales:502.19B, Div: -",
+ "MSFT: MC: 1794.80B, Emp:221000, Sales:203.07B, Div:2.72",
+  "TSLA: MC:337.58B, Emp:99290, Sales:74.86B, Div: - ")
+  return Stocks
